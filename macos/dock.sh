34788b5b1a4e98039908388998e1686d1bf22c60
Julien <ju@MacBook-Pro-de-Julien.local>
2021-07-25 15:37:50 +0200

    feat(mac): dock setup

--- macos/dock.sh
+++ macos/dock.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
 
 #dockutil --no-restart --remove all
-#dockutil --no-restart --add "/Applications/Google Chrome.app"
-#dockutil --no-restart --add "/System/Applications/Mail.app"
-#dockutil --no-restart --add "/System/Applications/Calendar.app"
+
+dockutil --no-restart --add "/System/Applications/Mail.app"
+dockutil --no-restart --add "/Applications/Google Chrome.app"
+dockutil --no-restart --add "/Applications/Slack.app"
+dockutil --no-restart --add "/Applications/Obsidian.app"
+dockutil --no-restart --add "/System/Applications/Calendar.app"
 #dockutil --no-restart --add "/System/Applications/Utilities/Terminal.app"
-#dockutil --no-restart --add "/System/Applications/System Preferences.app"
-#dockutil --no-restart --add "/Applications/Spotify.app"
+dockutil --no-restart --add "/System/Applications/System Preferences.app"
+dockutil --no-restart --add "/Applications/Spotify.app"
+dockutil --no-restart --add "/Applications/iTerm.app"
+dockutil --no-restart --add "/Applications/MacPass.app"
 
 killall Dock
